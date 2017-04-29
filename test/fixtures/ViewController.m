92c3f39d7a89fd0c18ec9fd65a61766e267fae63
liushulong <bigfish_liu@126.com>
2017-04-30 00:24:52 +0800

    init commit

---
+++ test/fixtures/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ONESetLanguageViewController.m
+//  Pods
+//
+//  Created 龙龙create
+//
+//
+
+#import "ONESetLanguageViewController.h"
+
+#define MyName @"龙defin"
+#pragma mark - 语言设置
+
+/**
+* 注释1
+  注释2
+*/
+    //1.上次和本次客户端和服务端选择相同
+    } else {//简体中文需要弹框
+            model.message = @"The car services displayed will change after switching to English. Would you like to continue?";
+            model.buttonTitles = @[@"确定",@"Cancel"];
+            string = @"字段1";  string = @"ddd字段2dd"
+    }
+
+}
+
+
+
+@end
